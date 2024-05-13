6c5c50d86da19c26bb9bf5c5f7b9f9dbbd33a6ba
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-13 20:23:46 +0300

    add task 2

--- js/task-2.js
+++ js/task-2.js
@@ -11,16 +11,12 @@ const images = [
     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
     alt: "Group of Horses Running",
   },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    alt: "Alpine Spring Meadows",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    alt: "Nature Landscape",
-  },
-  {
-    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    alt: "Lighthouse Coast Sea",
-  }
 ];
+
+const list = document.querySelector('.gallery');
+
+const markup = images
+    .map(({ url, alt}) => `<li><img src="${url}" alt="${alt}"></li>`)
+    .join('');
+
+list.insertAdjacentHTML('afterbegin', markup);
