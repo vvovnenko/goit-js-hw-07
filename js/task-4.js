23e177e4a951e785503c80b0dc49727956bd0d96
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-13 21:22:12 +0300

    add task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const inputNames = ['email', 'password'];
+const form = document.querySelector('form.login-form');
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formData = inputNames.reduce(
+        (acc, name) => {
+            return { ...acc, [name]: event.currentTarget.elements[name].value.trim()};
+        },
+        {},
+    );
+    if (Object.values(formData).some((value) => !value)) {
+        alert('All form fields must be filled in');
+    } else {
+        console.log(formData);
+        event.currentTarget.reset();
+    }
+}
+
+form.addEventListener('submit', onFormSubmit);
