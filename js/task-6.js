7520b5aa50a8a4bb77578effdb78b8d80fc3f240
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-13 21:36:40 +0300

    add task 6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBox(order) {
+  const box = document.createElement('div');
+  box.style.width = `${30 + order * 10}px`;
+  box.style.height = `${30 + order * 10}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+}
+
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('div#boxes');
+
+function createBoxes(amount) {
+  const boxesArray = [];
+  for (let i = 0; i < amount; i++) {
+    boxesArray.push(createBox(i));
+  }
+  boxes.append(...boxesArray);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  if (amount > 1 || amount < 100) {
+    destroyBoxes();
+    createBoxes(amount);
+  }
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
