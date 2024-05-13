32c755e9a84e695f94420d8a1fded35ddbccabf7
Volodymyr Vovnenko <vovnenko.volodymyr@gmail.com>
2024-05-13 21:24:18 +0300

    add task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector('button.change-color');
+const span = document.querySelector('span.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.style.color = color;
+  span.textContent = color;
+});
